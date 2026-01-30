5d518e319de4a762b0c1f9f17e9437b85956235c
VikasSutar1009 <vikassutarfsd1020@gmail.com>
2026-01-30 22:17:07 +0530

    Fixify project

--- Light/Dark Theme Toggle/script.js
+++
@@ -1,9 +0,0 @@
-const themeBtn = document.getElementById("themeBtn");
-const body = document.body;
-
-themeBtn.addEventListener("click",() => {
-    body.classList.toggle("dark-mode");
-    themeBtn.textContent = body.classList.contains("dark-mode")
-    ? "Switch to Light Mode"
-    : "Switch to Dark Mode";
-});
